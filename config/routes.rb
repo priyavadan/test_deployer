05bf2d9d16e1da8f5a671499b595bbb73208f18a
Priyavadan Kumar <priyavadan.kumar@nxp.com>
2018-05-24 08:12:59 -0700

    added Admin role, enabled authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   devise_for :users
+  
+  root to: 'welcome#index'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
 end
